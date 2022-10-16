7e2864c266c2ad06c8a2049971d97ebfa30eb305
Denis <de9404@gmail.com>
2022-10-16 15:40:48 +0300

    Task 1 is ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿Console.WriteLine("Привет, введите первое число: ");
-firstNum = Console.ReadLine();
+﻿int max = 0;
+int min = 0;
+Console.WriteLine("Привет, введите первое число: ");
+string strNum1 = Console.ReadLine();
+int firstNum = Convert.ToInt32(strNum1);
+Console.WriteLine("Введите Второе число: ");
+string strNum2 = Console.ReadLine();
+int secondNum = Convert.ToInt32(strNum2);
+if (firstNum > secondNum)
+{
+    max = firstNum;
+    min = secondNum;
+}
+else
+{
+    max = secondNum;
+    min = firstNum;
+}
+Console.WriteLine("max = "+ max + ", min = " + min);
