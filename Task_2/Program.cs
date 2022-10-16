0fdee881dd7208133a479da94273d68f0f0d105a
Denis <de9404@gmail.com>
2022-10-16 15:38:10 +0300

    second Task is resdy

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет, введите первое число: ");
+string strNum1 = Console.ReadLine();
+int firstNum = Convert.ToInt32(strNum1);
+Console.WriteLine("Введите Второе число: ");
+string strNum2 = Console.ReadLine();
+int secondNum = Convert.ToInt32(strNum2);
+Console.WriteLine("Введите третье число: ");
+string strNum3 = Console.ReadLine();
+int thirdNum = Convert.ToInt32(strNum3);
+
+int[] newArray  = { firstNum, secondNum, thirdNum };
+int max = newArray.Max();
+
+Console.WriteLine("max = "+ max);
