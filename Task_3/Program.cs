02723e14579a137a512e3c49b20e5a24471a2f2e
Denis <de9404@gmail.com>
2022-10-16 16:50:14 +0300

    Task 3 is ready

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет, введите первое число: ");
+string strNum1 = Console.ReadLine();
+int firstNum = Convert.ToInt32(strNum1);
+if ((firstNum%2) == 0)
+{
+    Console.WriteLine(firstNum + " - чётное число");
+}
+else 
+{
+    Console.WriteLine(firstNum + " - нечётное число");
+}
